7ba5b329ce0e4fe3c0a4dd191688946c08db9ee1
simonhac <simon@holmesacourt.com>
2017-02-08 20:51:00 +1100

    missed file and updated docs
    * accidentally omitted index.js
    * updated README

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 'use strict';
 
-var cbusUtils = require('./cbus-utils.js');
-var cbusClient = require('./cgate-client.js');
-var Service, Characteristic, Accessory, uuid;
+const CGateClient = require('./cgate-client.js');
+const CBusNetId = require('./cbus-netid.js');
+const cbusUtils = require('./cbus-utils.js');
 
-var CBusAccessory;
-var CBusLightAccessory;
-var CBusDimmerAccessory;
-var CBusMotionAccessory;
-var CBusSecurityAccessory;
-var CBusShutterAccessory;
-               
+let Service, Characteristic, Accessory, uuid;
+let CBusAccessory, CBusLightAccessory, CBusDimmerAccessory, CBusMotionAccessory, CBusSecurityAccessory, CBusShutterAccessory;
 
 //==========================================================================================
 //  Exports block
@@ -28,26 +23,23 @@ module.exports = function(homebridge) {
     //  Setup the CBus accessories
     //--------------------------------------------------
 
-    /* Load */
-    CBusAccessory        = require('./accessories/accessory.js')(Service, Characteristic, Accessory, uuid);
-    CBusLightAccessory   = require('./accessories/light-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
-    CBusDimmerAccessory  = require('./accessories/dimmer-accessory.js')(Service, Characteristic, CBusLightAccessory, uuid);
-    CBusMotionAccessory   = require('./accessories/motion-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
+    // load
+    CBusAccessory = require('./accessories/accessory.js')(Service, Characteristic, Accessory, uuid);
+    CBusLightAccessory = require('./accessories/light-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
+    CBusDimmerAccessory = require('./accessories/dimmer-accessory.js')(Service, Characteristic, CBusLightAccessory, uuid);
+    CBusMotionAccessory = require('./accessories/motion-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
     CBusSecurityAccessory = require('./accessories/security-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
     CBusShutterAccessory = require('./accessories/shutter-accessory.js')(Service, Characteristic, CBusAccessory, uuid);
 
-    /* Fix inheritance, since we've loaded our classes before the Accessory class has been loaded */
+    // fix inheritance, since we've loaded our classes before the Accessory class has been loaded
     cbusUtils.fixInheritance(CBusAccessory, Accessory);
     cbusUtils.fixInheritance(CBusLightAccessory, CBusAccessory);
     cbusUtils.fixInheritance(CBusDimmerAccessory, CBusLightAccessory);
     cbusUtils.fixInheritance(CBusMotionAccessory, CBusAccessory);
     cbusUtils.fixInheritance(CBusSecurityAccessory, CBusAccessory);
     cbusUtils.fixInheritance(CBusShutterAccessory, CBusAccessory);
-
-    //--------------------------------------------------
-    //  Register ourselfs with homebridge
-    //--------------------------------------------------
-
+	
+	// register ourself with homebridge
     homebridge.registerPlatform("homebridge-cbus", "CBus", CBusPlatform);
 };
 
@@ -59,61 +51,35 @@ function CBusPlatform(log, config) {
     //--------------------------------------------------
     //  vars definition
     //--------------------------------------------------
-    this.clientIpAddress     = undefined;
-    this.clientControlPort   = undefined;
-    this.clientEventPort     = undefined;
-    this.clientStatusPort    = undefined;
-    this.clientCbusName      = undefined;
-    this.clientNetwork       = undefined;
-    this.clientApplication   = undefined;
-    this.clientDebug         = undefined;
+	
     this.config	             = config;
     this.log                 = log;
     this.foundAccessories    = [];
     this.client              = undefined;
 
     //--------------------------------------------------
-    //  vars setup, required and default overrides
+    //  setup vars
     //--------------------------------------------------
 
-    /* Client IP */
-    if (typeof(config.client_ip_address) == "undefined" || typeof(config.client_cbusname) == "undefined") {
-        throw new Error('You must specify the client IP address and CBus name in your config file.');
+    // client IP and port
+    if (typeof(config.client_ip_address) === `undefined`) {
+        throw new Error('client IP address missing');
     }
-
-    this.clientIpAddress = config.client_ip_address;
-    this.clientCbusName = config.client_cbusname;
-
-    /* Client Control Port */
-    if (typeof(config.client_controlport) != "undefined") {
-      this.clientControlPort = config.client_contolport;
-    }
-
-    /* Client Event Port */
-    if (typeof(config.client_eventport) != "undefined") {
-      this.clientEventPort = config.client_eventport;
-    }
-
-    /* Client Status Port */
-    if (typeof(config.client_statusport) != "undefined") {
-      this.clientStatusPort = config.client_statusport;
-    }
-
-    /* Client Network Address */
-    if (typeof(config.client_network) != "undefined") {
-      this.clientNetwork = config.client_network;
-    }
-
-    /* Client Application Address */
-    if (typeof(config.client_application) != "undefined") {
-      this.clientApplication = config.client_application;
-    }
-
-    /* Client Debug */
-    if (typeof(config.client_debug) != "undefined") {
-      this.clientDebug = config.client_debug;
-    }
-};
+    this.cgateIpAddress = config.client_ip_address;
+	this.cgateControlPort = (typeof(config.client_contolport) !== 'undefined') ? config.client_contolport : undefined;
+	
+	// project name, network and default application
+	try {
+		this.project = CBusNetId.validatedProjectName(config.client_cbusname);
+	} catch (ex) {
+		throw new Error(`illegal client_cbusname`);
+	}
+	this.network = (typeof(config.client_network) !== `undefined`) ? config.client_network : undefined;
+	this.application = (typeof(config.client_application) !== `undefined`) ? config.client_application : undefined;
+		
+    // debug
+    this.clientDebug = (typeof(config.client_debug) != `undefined`) ? config.client_debug : false;
+}
 
 // Invokes callback(accessories[])
 CBusPlatform.prototype.accessories = function(callback) {
@@ -121,22 +87,28 @@ CBusPlatform.prototype.accessories = function(callback) {
     //  Initiate the CBus client
     //--------------------------------------------------
 
-    this.log.info("Connecting to the local CBus server...");
+    this.log.info("Connecting to the local C-Gate server...");
 
-    this.client = new cbusClient(this.clientIpAddress, this.clientControlPort, this.clientEventPort, this.clientStatusPort, this.clientCbusName, this.clientNetwork, this.clientApplication, this.clientDebug);
+    this.client = new CGateClient(this.cgateIpAddress, this.cgateControlPort,
+        this.project, this.network, this.application,
+        this.log, this.clientDebug);
 
     // listen for data from the client and ensure that the homebridge UI is updated
-    this.client.on("remoteData", function(data) {
+    this.client.on('remoteData', function(data) {
         if(this.clientDebug){
-            this.log.info("[remoteData] id:" + data.moduleId);
+            this.log.info("[remote] id:" + data.netId.moduleId);
         }
-        
+
+        // TODO change this to a map
+        if (typeof data.netId == undefined) {
+            this.log.info("[remote] :" + data.netId.moduleId);
+        }
         var devs = this.foundAccessories;
         for (var i = 0; i < devs.length; i++) {
             var dev = devs[i];
-            if(dev.id == data.moduleId) {
+            if(dev.id == data.netId.moduleId) {
                 if(this.clientDebug) {
-                    this.log.info("[remoteDataFound] id:" + data.moduleId + " type:" + dev.type + " level:" + data.level);
+                    this.log.info(`[remote] id: ${data.netId.moduleId} ${data.netId}, type: ${dev.type}, level: ${data.level}%`);
                 }
                 
                 // if we found a device, it must be supported
@@ -146,22 +118,22 @@ CBusPlatform.prototype.accessories = function(callback) {
     }.bind(this));
 
     this.client.connect(function() {
-        this.log.info('CBus Client is listening to CGate on ' + this.client.clientIpAddress 
         this.log.info("Registering the accessories list...");
         this.foundAccessories = []; /* reset */
 
-        for (var accessoryData of this.config.accessories) {
+        for (let accessoryData of this.config.accessories) {
             // make sure we use uuid_base so we dont see uuid collisions
-            accessoryData.uuid_base = accessoryData.id;
+            // unused? accessoryData.uuid_base = accessoryData.id;
 
-            var accessory = this.accessoryFactory(accessoryData);
-            if (accessory) {
-                this.foundAccessories.push(accessory);
-            } else {
-                this.log.error("Ignoring unknown accessory (type: %s).", accessoryData.type);
-            }
+			try {
+				let accessory = this.accessoryFactory(accessoryData);
+				this.foundAccessories.push(accessory);
+			} catch (ex) {
+				this.log.error(`Unable to instantiate accessory of type '${accessoryData.type}' (reason: ${ex}). ABORTING`);
+				process.exit(0);
+			}
         }
-
+        
         callback(this.foundAccessories.sort(function (a, b) {
             return (a.name > b.name) - (a.name < b.name);
         }));
@@ -176,15 +148,20 @@ CBusPlatform.prototype.accessoryFactory = function(entry) {
     switch (entry.type.toLowerCase()) {
         case "light":
             return new CBusLightAccessory(this, entry);
+            
         case "dimmer":
             return new CBusDimmerAccessory(this, entry);
+            
         case "motion":
             return new CBusMotionAccessory(this, entry);
+            
         case "security":
             return new CBusSecurityAccessory(this, entry);
+            
         case "shutter":
             return new CBusShutterAccessory(this, entry);
+            
         default:
-            return undefined;
+            throw `unknown accessory`;
     }
 };
