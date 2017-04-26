3be3fe1bb264ed86236e02420393369c8a1f2001
simonhac <simon@holmesacourt.com>
2017-04-26 14:37:31 +1000

    netId now handles project netIds

--- test/cbus-netid.test.js
+++ test/cbus-netid.test.js
@@ -19,7 +19,6 @@ test('constructor numerical', function (assert) {
 	assert.end();
 });
 
-
 test('constructor numerical', function (assert) {
 	assert.plan(4);
 
@@ -100,7 +99,7 @@ test('construct project address', function (assert) {
 });
 
 test('construct network address', function (assert) {
-	assert.plan(6);
+	assert.plan(5);
 
 	const netId = CBusNetId.parse(`//SHAC1/254`);
 	assert.equal(netId.project, 'SHAC1');
@@ -108,7 +107,6 @@ test('construct network address', function (assert) {
 	assert.equal(netId.application, undefined);
 	assert.equal(netId.group, undefined);
 	assert.equal(netId.toString(), `//SHAC1/254`);
-	assert.getHash(netId, 0xff0000)
 
 	assert.end();
 });
@@ -153,37 +151,6 @@ test('construct unit address', function (assert) {
 	assert.end();
 });
 
-test('construct illegal group address', function (assert) {
-	assert.plan(1);
-
-	assert.throws(function () {
-		CBusNetId.parse(`//SHAC/254/57/`);
-	});
-
-	assert.end();
-});
-
-test('construct illegal application address', function (assert) {
-	assert.plan(3);
-
-	assert.throws(function () {
-		// non numerical group
-		CBusNetId.parse(`//SHAC/254/57/abc`);
-	});
-
-	assert.throws(function () {
-		// non numerical group
-		CBusNetId.parse(`//SHAC/254/57/3.124`);
-	});
-
-	assert.throws(function () {
-		// non numerical group
-		CBusNetId.parse(`//SHAC/254a/57/3.124`);
-	});
-
-	assert.end();
-});
-
 test('construct malformed address', function (assert) {
 	assert.plan(1);
 
@@ -212,32 +179,21 @@ test('construct illegal whitespace', function (assert) {
 });
 
 test('construct legal project name', function (assert) {
-  assert.plan(1);
+	const addresses = [
+		{ in: '//HELLO/254/56/3', out: 'HELLO' },
+		{ in: '//3PROJECT/128/56/5', out: '3PROJECT' },
+		{ in: '//_PROJECT/254/56/6', out: '_PROJECT' },
+		{ in: '//MY_CBUS/254/56/1', out: 'MY_CBUS' },
+		{ in: '//7_LIGHT/254/54/56', out: '7_LIGHT' }
+	];
 
-  const addresses = [
-    '//HELLO/254/56/3',
-    '//3PROJECT/128/56/5',
-    '//_PROJECT/254/56/6',
-    '//MY_CBUS/254/56/1',
-    '//7_LIGHT/254/54/56'
-  ];
+	assert.plan(addresses.length);
 
-  var netIDs = addresses.map(function (s){
-    const netId = CBusNetId.parse(s);
-    return(netId.project);
-  });
+	addresses.forEach(o => {
+		assert.equal(CBusNetId.parse(o.in).project, o.out);
+	});
 
-  var result = [
-    'HELLO',
-		'3PROJECT',
-		'_PROJECT',
-		'MY_CBUS',
-    '7_LIGHT'
-	]
-
-	assert.deepEqual(netIDs, result);
-
-  assert.end();
+	assert.end();
 });
 
 test('construct illegal project name', function (assert) {
@@ -253,15 +209,15 @@ test('construct illegal project name', function (assert) {
 		CBusNetId.parse(`//S12345678/254/56`);
 	});
 
-  assert.throws(function () {
-    // cannot contain lowercase
-    CBusNetId.parse(`//project/254/56`);
-  });
+	assert.throws(function () {
+		// cannot contain lowercase
+		CBusNetId.parse(`//project/254/56`);
+	});
 
-  assert.throws(function () {
-    // cannot contain dash
-    CBusNetId.parse(`//MY-CBUS/254/56`);
-  });
+	assert.throws(function () {
+		// cannot contain dash
+		CBusNetId.parse(`//MY-CBUS/254/56`);
+	});
 
 	assert.throws(function () {
 		// name empty
@@ -277,7 +233,7 @@ test('construct illegal project name', function (assert) {
 		// reserved name
 		CBusNetId.parse(`//CBUS/254/56/34`);
 	});
-	
+
 	assert.throws(function () {
 		// reserved name
 		CBusNetId.parse(`//VM/254/56/34`);
@@ -292,7 +248,7 @@ test('construct illegal project name', function (assert) {
 		// reserved name
 		CBusNetId.parse(`//CGATE/254/56/34`);
 	});
-	
+
 	assert.throws(function () {
 		// reserved name
 		CBusNetId.parse(`//TAG/254/56/34`);
@@ -306,10 +262,170 @@ test('construct illegal project name', function (assert) {
 	assert.end();
 });
 
+test('construct illegal network address', function (assert) {
+	assert.plan(4);
+
+	assert.throws(function () {
+		// non numerical group
+		CBusNetId.parse(`//SHAC/ABC/57/22`);
+	}, /badly formed netid: '\/\/SHAC\/ABC\/57\/22'/);
+
+	assert.throws(function () {
+		// non numerical group
+		new CBusNetId(`SHAC1`, `ABC`, `57`, `22`);
+	}, /not an integer: 'ABC'/);
+
+	assert.throws(function () {
+		// out of bounds
+		CBusNetId.parse(`//SHAC/999/57/22`);
+	}, /network out of range: 999/);
+
+	assert.throws(function () {
+		// out of bounds
+		new CBusNetId(`SHAC`, `999`, `57`, `22`);
+	}, /network out of range: 999/);
+
+	assert.end();
+});
+
+test('construct illegal application address', function (assert) {
+	assert.plan(8);
+
+	assert.throws(function () {
+		// non numerical
+		CBusNetId.parse(`//SHAC/254/ABC/22`);
+	});
+
+	assert.throws(function () {
+		// non numerical
+		new CBusNetId(`SHAC`, `254`, `ABC`, `22`);
+	});
+
+	assert.throws(function () {
+		// non integer
+		CBusNetId.parse(`//SHAC/254/3.124/22`);
+	});
+
+	assert.throws(function () {
+		// non integer
+		new CBusNetId(`SHAC`, `254`, `3.124`, `22`);
+	}, /not an integer: '3.124'/);
+
+	assert.throws(function () {
+		// negative
+		CBusNetId.parse(`//SHAC/254/-22/22`);
+	});
+
+	assert.throws(function () {
+		// negative
+		new CBusNetId(`SHAC`, `254`, `-22`, `22`);
+	}, /application out of range: -22/);
+
+	assert.throws(function () {
+		// out of bounds
+		CBusNetId.parse(`//SHAC/254/999/22`);
+	});
+
+	assert.throws(function () {
+		// out of bounds
+		new CBusNetId(`SHAC`, `254`, `999`, `22`);
+	}, /application out of range: 999/);
+
+	assert.end();
+});
+
+test('construct illegal group address', function (assert) {
+	assert.plan(6);
+
+	assert.throws(function () {
+		// non numerical
+		CBusNetId.parse(`//SHAC/254/57/ABC`);
+	});
+
+	assert.throws(function () {
+		// non numerical
+		new CBusNetId(`SHAC`, `254`, `57`, `ABC`);
+	});
+
+	assert.throws(function () {
+		// non integer
+		CBusNetId.parse(`//SHAC/254/57/22.2`);
+	});
+
+	assert.throws(function () {
+		// non numerical
+		new CBusNetId(`SHAC`, `254`, `57`, `22.2`);
+	});
+
+	assert.throws(function () {
+		// out of bounds
+		CBusNetId.parse(`//SHAC/254/57/999`);
+	});
+
+	assert.throws(function () {
+		// non numerical
+		new CBusNetId(`SHAC`, `254`, `57`, `999`);
+	}, /group out of range: 999/);
+
+	assert.end();
+});
+
+test('construct illegal unit address', function (assert) {
+	assert.plan(10);
+
+	assert.throws(function () {
+		CBusNetId.parse(`//SHAC/254/p`);
+	});
+
+	assert.throws(function () {
+		new CBusNetId(`SHAC`, `254`, `p`);
+	});
+
+	assert.throws(function () {
+		// non numerical
+		CBusNetId.parse(`//SHAC/254/p/ABC`);
+	});
+
+	assert.throws(function () {
+		new CBusNetId(`SHAC`, `254`, `p`, `ABC`);
+	}, /not an integer: 'ABC'/);
+
+	assert.throws(function () {
+		// non integer
+		CBusNetId.parse(`//SHAC/254/p/22.2`);
+	});
+
+	assert.throws(function () {
+		new CBusNetId(`SHAC`, `254`, `p`, `22.2`);
+	}, /not an integer: '22.2'/);
+
+	assert.throws(function () {
+		// out of bounds
+		CBusNetId.parse(`//SHAC/254/p/999`);
+	});
+
+	assert.throws(function () {
+		// out of bounds
+		new CBusNetId(`SHAC`, `254`, `p`, `999`);
+	}, /unitAddress out of range: 999/);
+
+	assert.throws(function () {
+		// out of bounds
+		CBusNetId.parse(`//SHAC/254/p/-999`);
+	});
+
+	assert.throws(function () {
+		// out of bounds
+		new CBusNetId(`SHAC`, `254`, `p`, `-999`);
+	}, /unitAddress out of range: -999/);
+
+	assert.end();
+});
+
 test('getHash', function (assert) {
 	assert.plan(5);
 
-	assert.equal(CBusNetId.parse(`//S31415`).getHash(), `S31415`);
+	assert.equal(CBusNetId.parse(`//FOO`).getHash(), `FOO`);
 	assert.equal(CBusNetId.parse(`//S31415/254`).getHash(), `1fe0000`);
 	assert.equal(CBusNetId.parse(`//S31415/254/208`).getHash(), `1fed000`);
 	assert.equal(CBusNetId.parse(`//S31415/254/208/128`).getHash(), `1fed080`);
@@ -319,27 +435,35 @@ test('getHash', function (assert) {
 });
 
 test('CBusNetId equals', function (assert) {
-	assert.plan(2);
+	assert.plan(4);
 
 	const netId1 = CBusNetId.parse(`//S31415/254/208/128`);
 	const netId2 = CBusNetId.parse(`//S31415/254/208/128`);
 	assert.deepEquals(netId1, netId2);
+	assert.true(netId1.isEquals(netId2));
 
 	const netId3 = new CBusNetId(`S31415`, 254, 208, 128);
 	assert.deepEquals(netId2, netId3);
+	assert.true(netId2.isEquals(netId3));
 
 	assert.end();
 });
 
 test('CBusNetId not equals', function (assert) {
-	assert.plan(2);
+	assert.plan(6);
 
 	const netId1 = CBusNetId.parse(`//BAR/254/208/128`);
 	const netId2 = CBusNetId.parse(`//BAR/254/208/1`);
 	assert.notDeepEqual(netId1, netId2);
+	assert.false(netId1.isEquals(netId2));
 
 	const netId3 = new CBusNetId(`BAR`, 254, 208, 126);
 	assert.notDeepEqual(netId2, netId3);
+	assert.false(netId2.isEquals(netId3));
+
+	const netId4 = new CBusNetId(`FOO`, 254, 208, 126);
+	assert.notDeepEqual(netId3, netId4);
+	assert.false(netId3.isEquals(netId4));
 
 	assert.end();
 });
@@ -353,12 +477,11 @@ test('CBusNetId isProjectId', function (assert) {
 	assert.false(CBusNetId.parse(`//BAR/1/2/3`).isProjectId());
 	assert.false(CBusNetId.parse(`//BAR/1/p/3`).isProjectId());
 
-
 	assert.end();
 });
 
 test('CBusNetId isNetworkId', function (assert) {
-	assert.plan(4);
+	assert.plan(5);
 
 	assert.false(CBusNetId.parse(`//BAR`).isNetworkId());
 	assert.true(CBusNetId.parse(`//BAR/1`).isNetworkId());
@@ -370,7 +493,7 @@ test('CBusNetId isNetworkId', function (assert) {
 });
 
 test('CBusNetId isApplicationId', function (assert) {
-	assert.plan(4);
+	assert.plan(5);
 
 	assert.false(CBusNetId.parse(`//BAR`).isApplicationId());
 	assert.false(CBusNetId.parse(`//BAR/1`).isApplicationId());
@@ -382,7 +505,7 @@ test('CBusNetId isApplicationId', function (assert) {
 });
 
 test('CBusNetId isGroupId', function (assert) {
-	assert.plan(4);
+	assert.plan(5);
 
 	assert.false(CBusNetId.parse(`//BAR`).isGroupId());
 	assert.false(CBusNetId.parse(`//BAR/1`).isGroupId());
@@ -394,7 +517,7 @@ test('CBusNetId isGroupId', function (assert) {
 });
 
 test('CBusNetId isUnitId', function (assert) {
-	assert.plan(4);
+	assert.plan(5);
 
 	assert.false(CBusNetId.parse(`//BAR`).isUnitId());
 	assert.false(CBusNetId.parse(`//BAR/1`).isUnitId());
@@ -427,15 +550,15 @@ test('constructor undefineds', function (assert) {
 
 	assert.throws(function () {
 		new CBusNetId(`EXAMPLE`, undefined, 57);
-	}, /netIds with group must have a network/, `application no network`);
+	}, /netIds with application must have a network/, `application no network`);
 
 	assert.throws(function () {
 		new CBusNetId(`EXAMPLE`, undefined, 57, 22);
-	}, /netIds with group must have a network/, `group no network`);
+	}, /netIds with application must have a network/, `group no network`);
 
 	assert.throws(function () {
 		new CBusNetId(`EXAMPLE`, undefined, `p`, 22);
-	}, /netIds with group must have a network/, `unit no network`);
+	}, /unit netIds must have a network/, `unit no network`);
 
 	assert.throws(function () {
 		new CBusNetId(`EXAMPLE`, 254, undefined, 22);
